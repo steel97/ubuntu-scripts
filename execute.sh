dbea4eec9ecf50e4d58a3cdfeac2c5a0a7297b49
Ivan Yv <steel-97@mail.ru>
2021-05-04 22:40:08 +0300

    Updated scripts

--- execute.sh
+++ execute.sh
@@ -1,2 +1,4 @@
 sudo wget -O - http://example.com/script.sh && bash ./script.sh
 wget http://sitehere.com/install.sh -v -O install.sh && chmod 777 install.sh;./install.sh; rm -rf install.sh
+
+wget https://raw.githubusercontent.com/steel97/ubuntu-scripts/master/ubuntu-tightvnc.sh -v -O ubuntu-tightvnc.sh && chmod 777 ubuntu-tightvnc.sh;./ubuntu-tightvnc.sh; rm -rf ubuntu-tightvnc.sh
